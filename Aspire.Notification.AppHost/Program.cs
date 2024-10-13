f06efe3b96bf230a823de003674bcdbcf1e51867
ronithomas1 <ronithomas@gmail.com>
2024-10-13 12:27:13 +0530

    Adding Sql Server container

--- Aspire.Notification.AppHost/Program.cs
+++ Aspire.Notification.AppHost/Program.cs
@@ -1,4 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
+// Set a known password for the Sql server which for now is being read from the appsettings
+var sqlPassword = builder.AddParameter("sql-password");
+
+var sqlServer = builder
+                .AddSqlServer("sql", sqlPassword, port:1234)
+                .WithDataVolume("Aspire-Notification");
+// Adds intial catalog 'Aspire.Notification' to the connection string
+var sqlDatabase = sqlServer.AddDatabase("Aspire-Notification");
+
+
 var smtp = builder.AddSmtp4Dev("SmtpUri");
 builder.AddProject<Projects.Aspire_Notification_Api>("aspire-notification-api");
 
