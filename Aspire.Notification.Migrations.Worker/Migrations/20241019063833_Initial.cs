bf0637f3bf060f06ea58c5a21e1eb0c21e6dc419
ronithomas1 <ronithomas@gmail.com>
2024-10-19 12:38:20 +0530

    Implementing Migrations

---
+++ Aspire.Notification.Migrations.Worker/Migrations/20241019063833_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aspire.Notification.Migrations.Worker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Notification");
+
+            migrationBuilder.CreateTable(
+                name: "Template",
+                schema: "Notification",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    From = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NotificationTemplate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Template", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Template",
+                schema: "Notification");
+        }
+    }
+}
--- Aspire.Notification.Migrations.Worker/Program.cs
+++ Aspire.Notification.Migrations.Worker/Program.cs
@@ -1,8 +1,11 @@
 using Aspire.Notification.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Aspire.Notification.Migrations.Worker;
 
 // Refer this project for migrations sample
+// https://github.com/dotnet/aspire-samples/blob/main/samples/DatabaseMigrations/README.md
 // https://learn.microsoft.com/en-us/dotnet/aspire/database/ef-core-migrations
 public class Program
 {
@@ -16,7 +19,18 @@ public class Program
         .WithTracing(tracing => tracing.AddSource(DbInitializerWorker.ActivitySourceName));
 
         // This is the name of the sqlDatabase in AppHost
-        builder.AddSqlServerDbContext<AspireContext>("Aspire-Notification");
+        //  builder.AddSqlServerDbContext<AspireContext>("Aspire-Notification");
+        // https://github.com/dotnet/aspire-samples/blob/main/samples/DatabaseMigrations/README.md
+        builder.Services.AddDbContextPool<AspireContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("Aspire-Notification"), sqlOptions =>
+       {
+           sqlOptions.MigrationsAssembly("Aspire.Notification.Migrations.Worker");
+           // Workaround for https://github.com/dotnet/aspire/issues/1023
+           sqlOptions.ExecutionStrategy(c => new RetryingSqlServerRetryingExecutionStrategy(c));
+       }));
+        builder.EnrichSqlServerDbContext<AspireContext>(settings =>
+        // Disable Aspire default retries as we're using a custom execution strategy
+        settings.DisableRetry = true);
 
         var host = builder.Build();
         host.Run();
